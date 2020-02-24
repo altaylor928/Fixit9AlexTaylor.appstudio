1172999867e400869e44de897e559ecf08004506
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-02-24 14:57:48 -0600

    Change to membership form

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,13 @@
-/*
 var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+btnSubmit1.onclick = function(){
+  var isMember = members.includes(inptFirstName.value)
+  if (isMember == true){
+    lblCheck.value = (`${inptFirstName.value}, you are a member.`)
+  }
+  else{
+    lblCheck.value = (`${inptFirstName.value} you are not a member. We will make you one.`)
+    members.push(inptFirstName.value)
+    lblCheck.value = (`${inptFirstName.value}, you are now a member!`)
+  }
+}
